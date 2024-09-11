1ca706f831ca8b9c1c892d80377d8fc4575df5de
Abraao Brandao <abraaobritof10@gmail.com>
2024-09-11 12:27:42 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-do list project',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('To-do list project'),
+      ),
+      body: const Center(child: Text('Pressione o botão! teste')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: const Text('Você pressionou o botão!'),
+              action: SnackBarAction(
+                label: 'Fechar',
+                onPressed: () {
+                  // Code to execute.
+                },
+              ),
+            ),
+          );
+        },
+        child: const Icon(Icons.touch_app),
+      ),
+    );
+  }
+}
